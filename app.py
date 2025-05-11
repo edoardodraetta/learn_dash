e198d63053426220bbd8d959d88610a6b38337d6
edoardodraetta <edraetta@gmail.com>
2025-05-11 12:39:51 +0200

    Cherry picked to restore the app

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-"""Hello world dash app"""
-from dash import Dash, html
+"""A dash app with controls and callbacks"""
+import pandas as pd
+import plotly.express as px
+from dash import Dash, Input, Output, callback, dash_table, dcc, html
+import dash_bootstrap_components as dbc
 
-# Initialize the app (the dash constructor)
-app = Dash()
+# dcc = dash core components
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Initialize the app - incorporate a Dash Bootstrap theme
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # App components to display
-app.layout = [html.Div(children='Hello World')]
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+    ]),
+
+    dbc.Row([
+        dbc.RadioItems(
+            options=[{"label": x, "value": x} for x in ["pop", 'lifeExp', 'gdpPercap']],
+            value="lifeExp", inline=True, id='radio-buttons-final')
+    ]),
+    dbc.Row([
+            dbc.Col([
+                dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'}) # type: ignore
+            ], width=6),
+
+            dbc.Col([
+                dcc.Graph(figure={}, id='my-first-graph-final')
+            ], width=6),
+        ]),
+
+    ], fluid=True)
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='my-first-graph-final', component_property='figure'),
+    Input(component_id='radio-buttons-final', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
 
 # Run the app
 if __name__ == '__main__':
